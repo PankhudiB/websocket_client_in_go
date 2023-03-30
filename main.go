1a5cb62a1d588b88a57d71f8cefa6d179b534b76
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2023-03-30 22:11:10 +0530

    defer unmarshalling messages on websocket using json.RawMessage

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/websocket"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 	"log"
 )
 
@@ -11,21 +15,39 @@ func main() {
 	fmt.Println("Starting WebSocket Client...")
 
 	fmt.Println("Dialing to WebSocket Server...\n\n")
-	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	ctx := context.Background()
+	conn, _, _, err := ws.Dial(ctx, url)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-
-	//Sending message
-	if err := conn.WriteMessage(websocket.TextMessage, []byte("Hello from client!\n")); err != nil {
-		log.Fatal(err)
+	dataBytes, _, err := wsutil.ReadServerData(conn)
+	if err != nil {
+		fmt.Println("Error reading from websocket connection ! ", err.Error())
 	}
 
-	for {
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Print("Message:", string(message))
+	messageWrapper := MessageWrapper{}
+	err = json.Unmarshal(dataBytes, &messageWrapper)
+	if err != nil {
+		fmt.Println("Error unmarshalling...", err.Error())
 	}
+
+	fmt.Println("Level 1 Unmarshalling...", "\n", "Type : ", messageWrapper.MessageType, "\n", "Content : ", messageWrapper.Content)
+
+	subMessage := SubMessage{}
+	err = json.Unmarshal(messageWrapper.Content, &subMessage)
+	if err != nil {
+		fmt.Println("Error ", err.Error())
+	}
+
+	fmt.Println("\n\nLevel 2 Unmarshalling...", "\n", "subMessage.Name : ", subMessage.Name, "\n", "subMessage.Place : ", subMessage.Place)
+}
+
+type MessageWrapper struct {
+	MessageType string          `json:"message_type"`
+	Content     json.RawMessage `json:"content"`
+}
+
+type SubMessage struct {
+	Name  string `json:"name"`
+	Place string `json:"place"`
 }
